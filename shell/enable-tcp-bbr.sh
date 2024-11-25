d123a9fb5fcea460fd72ff5307bfe0c5448cf211
Mikhail Gunin <gunka462@gmail.com>
2024-11-25 19:18:30 +0300

    fix: make enable-tcp-bbr.sh idempotent

--- shell/enable-tcp-bbr.sh
+++ shell/enable-tcp-bbr.sh
@@ -2,6 +2,9 @@
 
 set -eo pipefail
 
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+grep -q "^net.core.default_qdisc=fq" /etc/sysctl.conf \
+    || echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+grep -q "^net.ipv4.tcp_congestion_control=bbr" /etc/sysctl.conf \
+    || echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+
 sysctl -p
